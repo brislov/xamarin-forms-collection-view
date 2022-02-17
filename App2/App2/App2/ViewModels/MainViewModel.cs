f8ce04a4c89d4cb2dc032b429ec84631eb657f4e
brislov <markusbrislov@gmail.com>
2022-02-17 22:50:05 +0100

    first commit

---
+++ App2/App2/App2/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App2.ViewModels
+{
+
+ 
+    public class MainViewModel : BindableObject
+    {
+        
+    }
+}
